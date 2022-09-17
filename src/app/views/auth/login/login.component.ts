a1808bb67b65294e752a40059801a1ffc65f559c
Fatare <kazukisamax@gmail.com>
2022-09-17 16:28:33 +0200

    17/09/2022

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+
+  user = {
+    email: 'a@a.com',
+    password: 'aaa',
+    name: 'aaa',
+    surname: 'a'
+  };
+
+  form = new FormGroup({
+    "email": new FormControl("", Validators.required),
+    "name": new FormControl("", Validators.required),
+    "surname": new FormControl("", Validators.required),
+    "password": new FormControl("", Validators.required),
+  });
+
+  onSubmit() {
+    console.log("reactive forn subitted");
+    console.log(this.form);
+  }
+
 
   public isClicked: boolean = true;
   constructor() { }
--- src/app/views/auth/signup/signup.component.ts
+++ src/app/views/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,10 +9,26 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SignupComponent implements OnInit {
 
-  
+  user = {
+    email: 'a@a.com',
+    password: 'aaa',
+    name: 'aaa',
+    surname: 'a'
+  };
+
+  form = new FormGroup({
+    "email": new FormControl("", Validators.required),
+    "name": new FormControl("", Validators.required),
+    "surname": new FormControl("", Validators.required),
+    "password": new FormControl("", Validators.required),
+  });
+
+  onSubmit() {
+    console.log("reactive forn subitted");
+    console.log(this.form);
+  }
 
   public isClicked: boolean = true;
-  signupUserData = {}
   constructor(private _auth: UserService) { }
 
   ngOnInit(): void {
