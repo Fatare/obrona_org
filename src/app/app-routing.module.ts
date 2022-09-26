1c025968d865b12dc1aee5642a77d4616d48ff49
Fatare <kazukisamax@gmail.com>
2022-09-26 16:30:38 +0200

    26/09/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/auth/login/login.component';
 import { SignupComponent } from './views/auth/signup/signup.component';
+import { PageComponent } from './views/page/page.component';
 
 const routes: Routes = [{ path: "Login", component: LoginComponent},
 { path: "Signup", component: SignupComponent},
+{ path: "landingPage", component: PageComponent},
 ];
 
 @NgModule({
