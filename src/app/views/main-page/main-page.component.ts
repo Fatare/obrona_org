f7d331d1b5e4517a98adbb0527f0f36e2f202cf7
Fatare <kazukisamax@gmail.com>
2022-10-03 20:40:14 +0200

    3/10/2022

--- src/app/views/main-page/main-page.component.ts
+++ src/app/views/main-page/main-page.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
+  templateUrl: './main-page.component.html', 
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+
+  products = [
+    {price: '2500,99', name:'Aparat', brand: 'ANEX', tax:'0,23'},
+    {price: '500,99', name:'Telefon', brand: 'NOKIOS', tax:'0,23' },
+    {price: '200,99', name:'Mikrofon', brand: 'YOTIK', tax:'0,23' }
+  ];
 
   constructor() { }
 
