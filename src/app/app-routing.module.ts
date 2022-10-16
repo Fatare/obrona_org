7cc75ad5efcefc29b7ad56e60d71e4ec3bf1d14f
Fatare <kazukisamax@gmail.com>
2022-10-16 12:33:48 +0200

    16/10/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './views/auth/login/login.component';
 import { SignupComponent } from './views/auth/signup/signup.component';
 import { MainPageComponent } from './views/main-page/main-page.component';
 import { ProductCartComponent } from './views/product-cart/product-cart.component';
 import { ShopCartComponent } from './views/shop-cart/shop-cart.component';
 
-const routes: Routes = [{ path: "Login", component: LoginComponent},
-{ path: "Signup", component: SignupComponent},
-{ path: "shopCart", component: ShopCartComponent},
+
+const routes: Routes = [
+  
+{ path: "shopCart", component: ShopCartComponent, children: [
 { path: "mainPage", component: MainPageComponent},
-{ path: "productCart", component: ProductCartComponent}
+{ path: "productCart", component: ProductCartComponent},
+]},
+
+{ path: "Login", component: LoginComponent},
+{ path: "Signup", component: SignupComponent},
+{ path: "", redirectTo: "Login", pathMatch: "full"},
+{ path: "**", redirectTo: "Login", pathMatch: "full"}
 ];
 
 @NgModule({
